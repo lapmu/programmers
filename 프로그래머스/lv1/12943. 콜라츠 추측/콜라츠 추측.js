0c6c810dbf523074c297dc3c1fd85bcc7bf531a0
lapmu <115691844+lapmu@users.noreply.github.com>
2023-04-27 22:50:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    let count = 0
+    while(num!==1){
+        if(count===500){
+            return -1;
+        }
+        if(num%2){
+            num = num * 3 + 1;
+        }else{
+            num = num / 2;
+        }
+        count++;
+    }
+    return count;
+}
