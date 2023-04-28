c7aec3ac536e39965029696e8d8018448784f70d
lapmu <115691844+lapmu@users.noreply.github.com>
2023-04-28 23:37:36 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,6 @@
+function solution(seoul) {
+    // 배열 매서드인 indexOf로 index값을 알아낸다
+    // 알아낸 index값을 반환형식에 맞춰 반환한다.
+    const indexNumber = seoul.indexOf('Kim');
+    return `김서방은 ${indexNumber}에 있다`
+}
