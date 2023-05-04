89e731d1a3c71a4b2051b113e974ae7b073eda22
lapmu <115691844+lapmu@users.noreply.github.com>
2023-05-04 13:54:25 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    // replace + 정규표현식으로 숫자 제거
+    // 위 과정을 거친 문자열이 빈문자열이 아닐경우 answer을 false로 할당
+    var answer = true;
+    if(s.length!==4&&s.length!==6){
+        return false;
+    }
+    const replacedS = s.replace(/[0-9]*/g, '');
+    if(replacedS.length!==0){
+        answer = false;
+    }
+    return answer;
+}
