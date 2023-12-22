8ee06d0955ac69a11828c7620ba762f9b49628b6
lapmu <115691844+lapmu@users.noreply.github.com>
2023-12-23 06:32:45 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,49 @@
+function solution(fees, records) {
+    const answer = [];
+    const sequence = []
+    
+    const time = records.map(el => {
+        const arr = el.split(' ');
+        arr[0] = arr[0].split(':');
+        arr[0] = arr[0][0] * 60 + +arr[0][1];
+        return arr
+    }).reduce((obj,el)=>{
+        const isInner = sequence.indexOf(el[1]);
+        if(isInner !== -1){
+            sequence.splice(isInner, 1);
+        }
+        
+        sequence.push(el[1])
+        
+        if(!obj[el[1]]){
+           obj[el[1]] = 0;
+        }
+        
+        if(el[2]==="IN"){
+           obj[el[1]] = obj[el[1]] - el[0]
+        }else{
+           obj[el[1]] = obj[el[1]] + el[0]
+           
+       }
+        return obj
+    }, {})
+    
+    sequence.sort((a,b)=>a-b)
+    
+    sequence.forEach(el => {
+        
+        if(time[el] <= 0){
+            time[el] = 1439 + time[el];
+        }
+            
+        if(time[el] <= fees[0]){
+            answer.push(fees[1])
+        }else{
+            const totalPrice = fees[1] + Math.ceil((time[el] - fees[0])/fees[2]) * fees[3]
+            answer.push(totalPrice)
+        }
+        
+    })
+    
+    return answer
+}
