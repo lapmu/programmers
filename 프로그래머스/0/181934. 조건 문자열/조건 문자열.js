75c0dac2646d19bc603c24e611de40b9ce7c65bf
lapmu <115691844+lapmu@users.noreply.github.com>
2025-08-07 20:14:28 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,28 @@
+function solution(ineq, eq, n, m) {
+    if(ineq==='<' && eq==='='){
+        if(n<=m){
+            return 1
+        }else{
+            return 0
+        }
+    }else if(ineq==='<'&& eq==='!'){
+        if(n<m){
+            return 1
+        }else{
+            return 0
+        }
+    }else if(ineq==='>'&& eq==='='){
+        if(n>=m){
+            return 1
+        }else{
+            return 0
+        }
+    }else if(ineq==='>'&& eq==='!'){
+        if(n>m){
+            return 1
+        }else{
+            return 0
+        }
+    }
+
+}
