39153fd460347351b522fd20b41e90fb145cce51
lapmu <115691844+lapmu@users.noreply.github.com>
2025-08-13 21:49:50 +0900

    [level 0] Title: 코드 처리하기, Time: 11.48 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,27 @@
+function solution(code) {
+    let answer = '';
+    let mode = 0;
+    for(let i = 0; i<code.length; i++){
+        if(mode===0){
+            if(code[i]==='1'){
+                mode = 1;
+
+            }else if(i%2===0){
+                answer += code[i];
+
+            }
+        }else{
+            if(code[i]==='1'){
+                mode = 0;
+
+            }else if(i%2===1){
+                answer += code[i];
+
+            }
+        }
+    }
+    if(answer.length===0){
+        answer = 'EMPTY'
+    }
+    return answer;
+}
