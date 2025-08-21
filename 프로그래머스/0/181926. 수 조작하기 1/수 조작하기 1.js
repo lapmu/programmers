3e533cf08cfebc6a267be038f513ea8009cd163f
lapmu <115691844+lapmu@users.noreply.github.com>
2025-08-21 20:18:12 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.96 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    var answer = n;
+    for(let i = 0; i<control.length; i++){
+        if(control[i]==='w'){
+            answer ++;
+        }else if(control[i]==='s'){
+            answer --;
+        }else if(control[i]==='d'){
+            answer += 10;
+        }else{
+            answer -= 10;
+        }
+    }
+    return answer;
+}
