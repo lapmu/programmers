ca08d5fa46de35f9d6e81ecc59b350c1ee473373
lapmu <115691844+lapmu@users.noreply.github.com>
2025-09-12 21:43:48 +0900

    [level 0] Title: 접미사 배열, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    var answer = [];
+
+    for(let i = 0; i < my_string.length; i++){
+
+        answer.push(my_string.slice(i-my_string.length, my_string.length));
+    }
+    answer.sort();
+    return answer;
+}
